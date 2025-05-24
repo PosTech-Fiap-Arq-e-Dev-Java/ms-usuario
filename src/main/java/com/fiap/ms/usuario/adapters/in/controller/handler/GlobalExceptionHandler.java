afa58043d1226de124f0947b0fccb92ca89b1d94
rayssec <rayssecutrim@hotmail.com>
2025-05-24 14:21:22 -0300

    Fix incluindo validacao de parametro obrigatório

--- src/main/java/com/fiap/ms/usuario/adapters/in/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/com/fiap/ms/usuario/adapters/in/controller/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -36,6 +37,18 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Object> handleMissingParams(MissingServletRequestParameterException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", OffsetDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Bad Request");
+        body.put("message", "O parâmetro '" + ex.getParameterName() + "' é obrigatório.");
+        body.put("path", request.getDescription(false).replace("uri=", ""));
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+    
     @ExceptionHandler(UsuarioJaExistenteException.class)
     public ResponseEntity<Object> handleUsuarioJaExistente(UsuarioJaExistenteException ex, WebRequest request) {
 
